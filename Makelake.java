044356feffcbcb657948243408c3a94ce15fe3e9
Sarose550 <43526712+Sarose550@users.noreply.github.com>
2019-03-06 10:58:33 -0500

    Update Makelake.java

--- Makelake.java
+++ Makelake.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
 public class Makelake{
+	public static void main(String[] args){
+		new Makelake().run();
+	}
+
+	private void run(){
+		Scanner sys = new Scanner("makelake.in");
+		int r = Integer.parseInt(sys.next());
+		int c = Integer.parseInt(sys.next());
+		int e = Integer.parseInt(sys.next());
+		int n = Integer.parseInt(sys.next());
+		int [][] farm = new int[r][c];
+		for(int i = 0; i < r; i++){
+			for(int j = 0; j < c; j++){
+				farm[i][j] = Integer.parseInt(sys.next());
+			}
+		}
+		//initializes the array
+		while(sys.hasNextLine()){
+			int rs = Integer.parseInt(sys.next());
+			int cs = Integer.parseInt(sys.next());
+			int ds = Integer.parseInt(sys.next());
+			
+		}
+	}
 }
