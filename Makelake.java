f86242b5ffd9d5ecf3f73fac60ecdca29c4698a6
Sarose550 <43526712+Sarose550@users.noreply.github.com>
2019-03-07 11:03:11 -0500

    Update Makelake.java

--- Makelake.java
+++ Makelake.java
@@ -5,11 +5,15 @@ public class Makelake{
 	}
 
 	private void run(){
-		Scanner sys = new Scanner("makelake.in");
+		Scanner sys = new Scanner("Makelake.java");
+		while(sys.hasNext()){
+			System.out.println(sys.next());
+		}
 		int r = Integer.parseInt(sys.next());
 		int c = Integer.parseInt(sys.next());
 		int e = Integer.parseInt(sys.next());
 		int n = Integer.parseInt(sys.next());
+		int tot = 0;
 		int [][] farm = new int[r][c];
 		for(int i = 0; i < r; i++){
 			for(int j = 0; j < c; j++){
@@ -22,13 +26,26 @@ public class Makelake{
 			int cs = Integer.parseInt(sys.next());
 			int ds = Integer.parseInt(sys.next());
 			int max = farm[rs][cs];
-			for(int i = -1; i <= 1; i++){
-				for(int j = -1; j <= 1; j++){
-					if(rs + i < r && cs + j < j){
-						int 
+			for(int k = -1; k <= 1; k++){
+				for(int l = -1; l <= l; l++){
+					if(rs + k < r && cs + l < c){
+						if(farm[rs + k][cs + l] > max) max = farm[rs + k][cs + l];
+					}
+				}
+				int depth = max - ds;
+				for(int m = -1; m <= 1; m++){
+					for(int p = -1; p <= 1; p++){
+						if(rs + m < r && cs + p < c){
+							if(farm[rs + m][cs + p] > depth){
+								tot += farm[rs + m][cs + p] - depth;
+								farm[rs + m][cs + p] = depth;
+							}
+						}
 					}
 				}
 			}
+			int result = tot * 72 * 72;
+			System.out.println(result);
 		}
 	}
 }
