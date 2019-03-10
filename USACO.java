8f8fefb50a091a9c1f9cbb79d52071565251eff2
Sarose550 <43526712+Sarose550@users.noreply.github.com>
2019-03-10 19:02:37 -0400

    Update USACO.java

--- USACO.java
+++ USACO.java
@@ -51,4 +51,48 @@ public class USACO{
 		int result = tot * 72 * 72;
 		return result;
 	}
+	public static int silver(String filename) throws FileNotFoundException{
+		File input = new File(filename);
+		Scanner sys = new Scanner(input);
+		int n = Integer.parseInt(sys.next());
+		int m = Integer.parseInt(sys.next());
+		int t = Integer.parseInt(sys.next());
+		char[][] farm = new char[n][m];
+		int[][][] info = new int[n][m][2];
+		for(int i = 0; i < n; i++){
+			String row = sys.next();
+			for(int j = 0; j < m; j++){
+				farm[i][j] = row.charAt(j);
+			}
+		}
+		int r1 = Integer.parseInt(sys.next()) - 1;
+		int c1 = Integer.parseInt(sys.next()) - 1;
+		int r2 = Integer.parseInt(sys.next()) - 1;
+		int c2 = Integer.parseInt(sys.next()) - 1;
+		info[r1][c1][0] = 1;
+		for(int x = 0; x < t; x++){
+			for(int a = 0; a < n; a++){
+				for(int b = 0; b < m; b++){
+					if(!(farm[a][b] == '*')){//don't spread path counts to trees so they're always 0
+						for(int y = -1; y <= 1; y++){
+							for(int z = -1; z <= 1; z++){
+								if(y * z == 0 && (y + z != 0)){
+									if(a + y < n && a + y >= 0 && b + z < m && b + z >= 0){//all the neighboring squares in bounds
+										info[a][b][1] += info[a + y][b + z][0];// add the previous path counts of all the neighboring squares
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			for(int c = 0; c < n; c++){
+				for(int d = 0; d < m; d++){
+					info[c][d][0] = info[c][d][1];
+					info[c][d][1] = 0;
+				}
+			}
+		}
+		return info[r2][c2][0];
+	}
 }
